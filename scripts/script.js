fc5fa1b31a91c2e21a09b5c0c8bc973575a420f5
mariatheodo <maria.theodo@gmail.com>
2017-04-05 15:23:15 +0300

    sets working editor

--- scripts/script.js
+++ scripts/script.js
@@ -27,9 +27,17 @@ function getSelectedText() {
 }
 
 var phpCode = [new HelpText("echo", "<em>void</em> echo string, <br />εμφανίζει το string στην οθόνη, δεν χρειάζεται παρενθέσεις"),
-		new HelpText("for", "<strong>Παράδειγμα:</strong> <br /> <span class='code'>for($i = 0; $i < 10; $i++) {<br><span class='intend1'>echo $i;</span> <br>}</span>")];
+		new HelpText("for", "<strong>Παράδειγμα:</strong> <br /> <span class='code'>for($i = 0; $i < 10; $i++) {<br><span " +
+			"class='intend1'>echo $i;</span> <br>}</span>")];
 
 function HelpText(key, help) {
 	this.key = key;
 	this.help = help;
 }
+
+var editor = CodeMirror.fromTextArea(document.getElementsByTagName("textarea")[0], {
+	lineNumbers: true,
+	mode: "application/x-httpd-php",
+	value: "<?php\n",
+	lineWrapping: true
+});
