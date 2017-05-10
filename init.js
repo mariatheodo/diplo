52362c9dc97535777284568df918d656b18326f4
mariatheodo <maria.theodo@gmail.com>
2017-05-10 15:48:30 +0300

    initialize for codiad

--- init.js
+++ init.js
@@ -9,75 +9,94 @@
         scripts= document.getElementsByTagName('script'),
         path = scripts[scripts.length-1].src.split('?')[0],
         curpath = path.split('/').slice(0, -1).join('/')+'/';
+    console.log(path + " ..... " + curpath);
 
     // Instantiates plugin
     $(function() {    
-        codiad.learningTool.init();
+        codiad.LearningTool.init();
     });
 
-    codiad.learningTool = {
+    codiad.LearningTool = {
         
         // Allows relative `this.path` linkage
         path: curpath,
 
         init: function() {
-            $(document).ready(function() {
-                $(".ace_line").select(function () {
-                    text = this.getSelectedText().trim();
-                    $(".ace_content").append("<div id='helper'></div>");
-                    for (var i = 0; i < phpCode.length; i++) {
-                        if (phpCode[i].key === text) {
-                            $("#helper").html(phpCode[i].help);
-                            return;
-                        }
-                        else {
-                            $("#helper").html("");
-                        }
+
+            // Start your plugin here...
+            var _this       = this;
+            amplify.subscribe('active.onOpen', function(path){
+                _this.addKeyBindings();
+            });
+
+        },
+
+        /**
+         * 
+         * This is where the core functionality goes, any call, references,
+         * script-loads, etc...
+         * 
+         */
+        addKeyBindings: function() {
+            var _this = this;
+            if (codiad.editor.getActive() !== null) {
+                //Add initial keyboard command
+                var _commandManager = codiad.editor.getActive().commands;
+                _commandManager.addCommand({
+                    name: 'PHP Learning Tool',
+                    bindKey: {
+                        "win": "Ctrl-Alt-L"
+                    },
+                    exec: function () {
+                        _this.showHelp();
                     }
                 });
-            });
+            }
         },
 
-        getSelectedText: function() {
-        var text = "";
-        var activeElem = document.activeElement;
-        var activeElemClassName = activeElem.className ? activeElem.className.toLowerCase() : null;
-        if ((activeElemClassName == "ace_line") || (activeElemClassName == "ace_layer" &&
-            /^(?:text|search|password|tel|url)$/i.test(activeElem.type)) &&
-            (typeof activeElem.selectionStart == "number")) {
-            text = activeElem.value.slice(activeElem.selectionStart, activeElem.selectionEnd);
-        }
-        else if (window.getSelection) {
-            text = window.getSelection().toString();
-        }
-        return text;
+        showHelp: function() {
+            //alert("test passed");
+            var selectedText = codiad.editor.getSelectedText().toLowerCase().trim();
+            if(selectedText == "") {
+                return;
+            }
+            if(this.phpCode.hasOwnProperty(selectedText)) {
+                alert(this.phpCode[selectedText]);
+            }
+
         },
 
-        SOME_METHOD: function() {
-            alert('Hello World');
+        test: function() {
+            alert("what is this?");
         },
 
-        helpText: function (key, help) {
-            this.key = key;
-            this.help = help;
-        },
-
-        var phpCode = [new HelpText("echo", "<p>Η echo εμφανίζει το string στην οθόνη, δεν χρειάζεται παρενθέσεις.</p><p><h4>Παράδειγμα</h4></p>" +
-            "<p class='code'>echo 'Hello World';</p>"),
-            new HelpText("for", "<p><h4>Παράδειγμα</h4></p><p class='code'>for($i = 0; $i < 10; $i++) {</p><p class='code intend1'>" +
-                "echo $i;</p><p class='code'> }</p>"),
-            new HelpText("print", "<p>Η print() είναι μια συνάρτηση που στέλνει τα περιεχόμενα της παραμέτρου στον browser.</p>" +
-                "<p><span class='code'>print('Hello World');</span></p><p>Μπορεί επίσης να δουλέψει" +
-                " χωρίς παρενθέσεις.</p><p class='code'>print 'Παράδειγμα';</p><p class='code'>" +
-                "$foo = 'Καλημέρα';</p><p class='code'>print $foo;</p><p class='code'>$name = 'Maria';</p><p class='code'>" +
-                "print 'My name is $name';</p>"),
-            new HelpText("$GLOBALS", "<p>Η $GLOBALS περιέχει τα ονόματα και τις τιμές όλων των εσωτερικών μεταβλητών. Για να τις δείτε" +
-                " γράψτε:</p><p class='code'>print_r($GLOBALS);</p>"),
+        phpCode: {
+            "echo": "<p>Η echo εμφανίζει το string στην οθόνη, δεν χρειάζεται παρενθέσεις.</p><p><h4>Παράδειγμα</h4></p>" +
+            "<p class='code'>echo 'Hello World';</p>",
+            "for": "<p><h4>Παράδειγμα</h4></p><p class='code'>for($i = 0; $i < 10; $i++) {</p><p class='code intend1'>" +
+            "echo $i;</p><p class='code'> }</p>",
+            "print": "<p>Η print() είναι μια συνάρτηση που στέλνει τα περιεχόμενα της παραμέτρου στον browser.</p>" +
+            "<p><span class='code'>print('Hello World');</span></p><p>Μπορεί να δουλέψει και" +
+            " χωρίς παρενθέσεις.</p><p class='code'>print 'Παράδειγμα';</p><p class='code'>" +
+            "$foo = 'Καλημέρα';</p><p class='code'>print $foo;</p><p class='code'>$name = 'Maria';</p><p class='code'>" +
+            "print 'My name is $name';</p>",
+            "$globals": "<p>Η $GLOBALS περιέχει τα ονόματα και τις τιμές όλων των εσωτερικών μεταβλητών. Για να τις δείτε" +
+            " γράψτε:</p><p class='code'>print_r($GLOBALS);</p>",
+            "define": "<p>Η define() χρησιμοποιείται για ορισμό σταθερών</p><p class='code'>define('PI', 3.14);</p>",
+            "defined": "<p>Επιστρέφει true αν η σταθερά είναι δηλωμένη</p><p class='code'>if (defined('FIRSTNAME')) {</p>" +
+            "<p class='code intend1'>echo 'Welcome'.FIRSTNAME;</p><p class='code'>}</p>",
+            "if": "<p>Χρησιμοποιείται για έλεγχο μιας συνθήκης.</p><p class='code'>if ($a > 0) {</p><p class='code intend1'>" +
+            "//κώδικας</p><p class='code'>}</p><p class='code'>elseif ($a < 0) {</p><p class='code intend1'>//κώδικας</p>" +
+            "<p class='code'>}</p><p class='code'>else {</p><p class='code intend1'>//κώδικας</p><p class='code'>}</p><p>Τα σκέλη if " +
+            "και else if δεν είναι απαραίτητα.</p>"
             //new HelpText("$_POST", "<p>Με την $_POST μεταφέρονται δεδομένα που υποβάλει ο χρήστης μέσα από μια HTML φόρμα. Αν στη " +
             //"φόρμα έχουμε:</p><p class='code'>\<input method='post' name='onoma'\></p><p>τότε στην PHP θα τα δούμε ως:</p>")
-        ]
-    };
+        },
+
+        addToPhpCode: function(property, definition) {
+            this.phpCode[property] = definition;
+        }
+
+    }
 
 })(this, jQuery);
-}
-
