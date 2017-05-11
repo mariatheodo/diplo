9f8952077c934206098d7b7484afad2f00d0cda0
mariatheodo <maria.theodo@gmail.com>
2017-05-11 23:13:07 +0300

    opens new panel in workspace

--- init.js
+++ init.js
@@ -9,7 +9,6 @@
         scripts= document.getElementsByTagName('script'),
         path = scripts[scripts.length-1].src.split('?')[0],
         curpath = path.split('/').slice(0, -1).join('/')+'/';
-    console.log(path + " ..... " + curpath);
 
     // Instantiates plugin
     $(function() {    
@@ -20,11 +19,15 @@
         
         // Allows relative `this.path` linkage
         path: curpath,
+        isVisible: false,
+        panel: $('<div id="help">'),
 
         init: function() {
 
             // Start your plugin here...
             var _this       = this;
+            $('#workspace').append(panel);
+            panel.hide();
             amplify.subscribe('active.onOpen', function(path){
                 _this.addKeyBindings();
             });
@@ -56,45 +59,68 @@
 
         showHelp: function() {
             //alert("test passed");
-            var selectedText = codiad.editor.getSelectedText().toLowerCase().trim();
-            if(selectedText == "") {
-                return;
+            if (codiad.filemanager.getExtension(codiad.active.getPath()) == "php") {
+                var selectedText = codiad.editor.getSelectedText().toLowerCase().trim();
+                if(selectedText == "") {
+                    return;
+                }
+                if(this.phpCode.hasOwnProperty(selectedText)) {
+                    this.showPanel(selectedText);
+                }
             }
-            if(this.phpCode.hasOwnProperty(selectedText)) {
-                alert(this.phpCode[selectedText]);
-            }
-
-        },
-
-        test: function() {
-            alert("what is this?");
         },
 
         phpCode: {
             "echo": "<p>Η echo εμφανίζει το string στην οθόνη, δεν χρειάζεται παρενθέσεις.</p><p><h4>Παράδειγμα</h4></p>" +
-            "<p class='code'>echo 'Hello World';</p>",
+                "<p class='code'>echo 'Hello World';</p>",
             "for": "<p><h4>Παράδειγμα</h4></p><p class='code'>for($i = 0; $i < 10; $i++) {</p><p class='code intend1'>" +
-            "echo $i;</p><p class='code'> }</p>",
+                "echo $i;</p><p class='code'> }</p>",
             "print": "<p>Η print() είναι μια συνάρτηση που στέλνει τα περιεχόμενα της παραμέτρου στον browser.</p>" +
-            "<p><span class='code'>print('Hello World');</span></p><p>Μπορεί να δουλέψει και" +
-            " χωρίς παρενθέσεις.</p><p class='code'>print 'Παράδειγμα';</p><p class='code'>" +
-            "$foo = 'Καλημέρα';</p><p class='code'>print $foo;</p><p class='code'>$name = 'Maria';</p><p class='code'>" +
-            "print 'My name is $name';</p>",
+                "<p><span class='code'>print('Hello World');</span></p><p>Μπορεί να δουλέψει και" +
+                " χωρίς παρενθέσεις.</p><p class='code'>print 'Παράδειγμα';</p><p class='code'>" +
+                "$foo = 'Καλημέρα';</p><p class='code'>print $foo;</p><p class='code'>$name = 'Maria';</p><p class='code'>" +
+                "print 'My name is $name';</p>",
             "$globals": "<p>Η $GLOBALS περιέχει τα ονόματα και τις τιμές όλων των εσωτερικών μεταβλητών. Για να τις δείτε" +
-            " γράψτε:</p><p class='code'>print_r($GLOBALS);</p>",
+                " γράψτε:</p><p class='code'>print_r($GLOBALS);</p>",
             "define": "<p>Η define() χρησιμοποιείται για ορισμό σταθερών</p><p class='code'>define('PI', 3.14);</p>",
             "defined": "<p>Επιστρέφει true αν η σταθερά είναι δηλωμένη</p><p class='code'>if (defined('FIRSTNAME')) {</p>" +
-            "<p class='code intend1'>echo 'Welcome'.FIRSTNAME;</p><p class='code'>}</p>",
+                "<p class='code intend1'>echo 'Welcome'.FIRSTNAME;</p><p class='code'>}</p>",
             "if": "<p>Χρησιμοποιείται για έλεγχο μιας συνθήκης.</p><p class='code'>if ($a > 0) {</p><p class='code intend1'>" +
-            "//κώδικας</p><p class='code'>}</p><p class='code'>elseif ($a < 0) {</p><p class='code intend1'>//κώδικας</p>" +
-            "<p class='code'>}</p><p class='code'>else {</p><p class='code intend1'>//κώδικας</p><p class='code'>}</p><p>Τα σκέλη if " +
-            "και else if δεν είναι απαραίτητα.</p>"
-            //new HelpText("$_POST", "<p>Με την $_POST μεταφέρονται δεδομένα που υποβάλει ο χρήστης μέσα από μια HTML φόρμα. Αν στη " +
+                "//κώδικας</p><p class='code'>}</p><p class='code'>elseif ($a < 0) {</p><p class='code intend1'>//κώδικας</p>" +
+                "<p class='code'>}</p><p class='code'>else {</p><p class='code intend1'>//κώδικας</p><p class='code'>}</p><p>Τα σκέλη elseif " +
+                "και else δεν είναι απαραίτητα.</p>"
+            //new HelpText("$_post", "<p>Με την $_POST μεταφέρονται δεδομένα που υποβάλει ο χρήστης μέσα από μια HTML φόρμα. Αν στη " +
             //"φόρμα έχουμε:</p><p class='code'>\<input method='post' name='onoma'\></p><p>τότε στην PHP θα τα δούμε ως:</p>")
         },
 
         addToPhpCode: function(property, definition) {
             this.phpCode[property] = definition;
+        },
+
+        showPanel: function (text) {
+            if(!this.isVisible) {
+                this.isVisible = true;
+            }
+            panel.css({
+                'top': window.outerHeight * 0.2,
+                'left': window.outerWidth * 0.7,
+                'background-color': 'black',
+                'position': 'absolute',
+                'z-index': 200,
+                'display': 'inherit'
+            });
+
+            panel.show();
+            panel.html(this.phpCode[text]);
+            console.log("is it?");
+        },
+
+        hidePanel: function() {
+            if (this.isVisible) {
+                this.isVisible = false;
+                panel.html("");
+                panel.hide();
+            }
         }
 
     }
